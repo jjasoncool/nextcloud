e04e5d33a7fda4e2e6c5f6853c25ef5997d30037
jason <jason@localhost.localdomain>
2023-11-23 14:50:57 +0800

    feat: bump onlyoffice to 7.5.1

--- set_config.sh
+++ set_config.sh
@@ -12,5 +12,6 @@ docker exec -u www-data nextcloud php occ --no-warnings app:install onlyoffice
 docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice/"
 docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice jwt_secret --value="$OFFICE_PASS"
 
-docker exec -u www-data nextcloud php occ --no-warnings config:system:set allow_local_remote_servers  --value=true
+docker exec -u www-data nextcloud php occ --no-warnings config:system:set allow_local_remote_servers --value=true
