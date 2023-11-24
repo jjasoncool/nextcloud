77bf499fd269726263a004236f0bc027f1b781f7
jason <jason@localhost.localdomain>
2023-11-24 11:47:46 +0800

    chore: add setting configs commands

--- set_config.sh
+++ set_config.sh
@@ -14,4 +14,7 @@ docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyof
 docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
 docker exec -u www-data nextcloud php occ --no-warnings config:system:set onlyoffice jwt_secret --value="$OFFICE_PASS"
 
+docker exec -u www-data nextcloud php occ --no-warnings app:install drawio
+docker exec -u www-data nextcloud php occ --no-warnings config:system:set drawio DrawioUrl --value="http://$HOST_DOMAIN/drawio/"
+
 docker exec -u www-data nextcloud php occ --no-warnings config:system:set allow_local_remote_servers --value=true
